8efcbc8244882b190fd50dd21dd49351387fb0d5
Maurice Kratz <kratzkodes@gmail.com>
2023-08-14 19:19:24 -0400

    added task 4 and 5

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,27 @@
-const Buttons = () => {
+// import satData from "./satData";
+import React from "react";
+import satData from "./satData";
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
+  displaySats = [...displaySats];
+
+  function SatHeightDataSorting() {
+    let orbitHeightBttns = displaySats.map((sat, id) => {
+      return (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      );
+    });
+
+    return orbitHeightBttns;
+  }
+
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      <SatHeightDataSorting />
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,30 @@
 const Table = ({ sat }) => {
+
+
+
   return (
-      <table>
-       <thead>
+    <table>
+      <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
-        </tbody>
-      </table>
+      </thead>
+      <tbody>
+        {sat.map((sat,id) => {
+          return (
+            <tr>
+              <td>{sat.name}</td>
+              <td>{sat.type}</td>
+              <td>{sat.launchDate}</td>
+              <td>{sat.operational ? "Active" : "Inactive" }</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
