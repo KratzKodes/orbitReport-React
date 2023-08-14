906bfd8c853076e739011acc60cc4361d58af367
Maurice Kratz <kratzkodes@gmail.com>
2023-08-14 15:03:58 -0400

    created the banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class Banner extends Component {
+    render(){
+        return (
+          <div>
+            <header>
+              <h1>Orbit Report</h1>
+            </header>
+            <p>Click the corresponding button of the data you would like to view</p>
+          </div>
+        );
+    }
+}
